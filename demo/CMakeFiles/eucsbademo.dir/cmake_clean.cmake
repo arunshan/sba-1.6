bfd7851d8a7bf215601bc4eb10b661b3e8b38e9a
Arun Shankar <arun@fuiszmedia.com>
2016-11-11 21:13:44 -0800

    added compiled files

---
+++ demo/CMakeFiles/eucsbademo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eucsbademo.dir/eucsbademo.c.o"
+  "CMakeFiles/eucsbademo.dir/imgproj.c.o"
+  "CMakeFiles/eucsbademo.dir/readparams.c.o"
+  "eucsbademo.pdb"
+  "eucsbademo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eucsbademo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
